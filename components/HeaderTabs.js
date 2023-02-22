32c07621306ee99583337c83b2d2a13e836fb701
del-ui <wambuivictor@students.must.ac.ke>
2023-02-22 10:31:35 +0300

    header

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -4,21 +4,21 @@ import React from 'react'
 export default function HeaderTabs() {
   return (
     <View style={{flexDirection: 'row', alignSelf: 'center'}}>
-        <HeaderButton text="Delivery"  />
-        <HeaderButton text="Pickup" />
+        <HeaderButton text="Delivery" btncolor="black" textColor="white"  />
+        <HeaderButton text="Pickup" btncolor="white" textColor="black" />
     </View>
   );
 }
 const HeaderButton = (props) => (
    
         <TouchableOpacity style={{
-            backgroundColor: "black",
+            backgroundColor: props.btncolor,
             paddingVertical: 6,
             paddingHorizontal: 16,
             borderRadius: 30,
             marginTop: 25
         }}>
-            <Text style={{color: "white", fontSize: 15, fontWeight: "900"}}>{props.text}</Text>
+            <Text style={{color: props.textColor, fontSize: 15, fontWeight: "900"}}>{props.text}</Text>
         </TouchableOpacity>  
 
     
