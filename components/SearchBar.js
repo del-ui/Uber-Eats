bac6fe79210bbbd54625a3fe7108cd56d664a25a
del-ui <wambuivictor@students.must.ac.ke>
2023-02-26 13:32:06 +0300

    update

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 export default function SearchBar() {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View style={{marginTop: 15, flexDirection: 'row'}}>
+      <GooglePlacesAutocomplete placeholder='Search'/>
     </View>
   )
 }
