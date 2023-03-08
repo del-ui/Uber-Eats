888bbd35b8da719fd563b531a762288ebfc43700
del-ui <wambuivictor@students.must.ac.ke>
2023-03-08 10:05:19 +0300

    commit

--- components/Categories.js
+++ components/Categories.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
 
 const items = [
@@ -35,8 +35,33 @@ const items = [
 
 export default function Categories() {
   return (
-    <View>
-      <Image source={items[0].image} />
+    
+<View 
+style={{
+  marginTop:5,
+  backgroundColor: "#fff",
+  paddingVertical: 10,
+  paddingLeft: 20
+}}>
+
+<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      
+    {items.map((item, index) =>(
+      <View key={index} style={{alignItems: 'center', marginRight: 30}}>
+      <Image
+       source={item.image} 
+        style={{
+          width: 50,
+          height: 40,
+          resizeMode: "contain",
+      }} />
+      <Text style={{fontSize: 13, fontWeight: "900"}}>{item.text}</Text>
     </View>
-  )
+    ))}
+  
+    </ScrollView>
+
+</View>
+
+  );
 }
