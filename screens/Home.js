9a4569fea2d5fa1809f178fea0599e3d050553e3
del-ui <wambuivictor@students.must.ac.ke>
2023-03-12 10:20:14 +0300

    api-connect

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,35 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
 import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
 
+
+const YELP_API_KEY = "fsq3hZlANP46yxsFmRdbZD8Muh+mMj2BO3eEFBWFaA8WGno="
+
 export default function Home() {
-  const [restaurantData, setRestaurantData] = React.useState(localRestaurants)
+  const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+
+  const getRestaurantFromYelp = () => {
+    const yelpUrl = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=San+Francisco';
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+  return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => {
+        setRestaurantData(json.businesses);
+      });          
+  };
+  useEffect(()=> {
+    getRestaurantFromYelp();
+  } , []);
+  
   return (
     <SafeAreaView style={{backgroundColor: "#eee", flex: 1}}>
 
